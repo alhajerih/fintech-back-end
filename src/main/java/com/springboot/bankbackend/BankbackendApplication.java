82f90e6213046fb7ef9ee6ff601e888023d034c5
momadAB <mabaqer99@gmail.com>
2024-11-19 12:25:47 +0300

    Initial make by spring.io

---
+++ src/main/java/com/springboot/bankbackend/BankbackendApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.bankbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/bankbackend/BankbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.bankbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
